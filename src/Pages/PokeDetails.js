d7d6957e6e7575a7aef63eb38633cc2f25eea6c9
ZakAP <phillipszak@hotmail.com>
2021-07-06 19:41:12 -0300

    Esqueleto da página

---
+++ src/Pages/PokeDetails.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function PokeDetails() {
+    return (
+      <div>
+       
+      </div>
+    );
+}
+  
+export default PokeDetails;
---
+++ src/Pages/Pokedex.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Pokedex() {
+    return (
+      <div>
+       
+      </div>
+    );
+}
+  
+export default Pokedex;
