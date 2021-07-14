9b03d4a4aea81161cb866e2b2b5c4f60197038e4
eddvaz <edivaldo.vaz@hotmail.com>
2021-07-14 16:46:56 -0300

    tentando-renderizar-pkm-na-tela

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,12 +1,36 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import axios from "axios"
+
+
 
 
 function Home() {
 
+  const [pokemons, setPokemon] = useState()
+  
+
+  useEffect(() => {
+    axios
+      .get("https://pokeapi.co/api/v2/pokemon?offset=20&limit=20")
+      .then((response) =>{
+        setPokemon(response.data)
+      })
+  }, [])
+  
+  console.log(pokemons && pokemons.results)
+  
+ 
+
   return (
     <div>
       <h1>Home</h1>
-      
+      {pokemons && pokemons.results.map((pokemon)=>{
+        return(
+          <div>
+            {pokemon.name}
+          </div>
+        )
+      })}
     </div>
   );
 }
