d3268831165cf13bf30d3530e9368fdc8ed7c328
ZakAP <phillipszak@hotmail.com>
2021-07-15 04:21:04 -0300

    Funcionalidade da página de detalhes pronta

--- src/Pages/PokeDetails.js
+++ src/Pages/PokeDetails.js
@@ -13,43 +13,42 @@ function PokeDetails() {
       .get(`https://pokeapi.co/api/v2/pokemon/${pokemon}`)
       .then((response) =>{
         setPokeData(response.data)
-        console.log(response.data.sprites)
+        console.log(response.data.stats[0].base_stat)
       })
   }, [])
-
 
   return (
     <>
     {pokeData && <div>
       <h1>{pokemon}</h1>
       <section>
-        <img src="" alt={pokemon}/>
-        <img src="" alt={pokemon}/>
+        <img src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokeData.id}.png`} alt={`${pokemon} front`}  width="150" height="150"/>
+        <img src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/back/${pokeData.id}.png`} alt={`${pokemon} back`}  width="150" height="150"/>
       </section>
 
       <section>
         <h2>Status</h2>
-        <p>HP: </p>
-        <p>Ataque: </p>
-        <p>Defesa: </p>
-        <p>Ataque-Especial: </p>
-        <p>Defesa-Especial: </p>
-        <p>Velocidade</p>
+        <p><b>HP:</b> {pokeData.stats[0].base_stat}</p>
+        <p><b>Ataque:</b> {pokeData.stats[1].base_stat}</p>
+        <p><b>Defesa:</b> {pokeData.stats[2].base_stat}</p>
+        <p><b>Ataque-Especial:</b> {pokeData.stats[3].base_stat}</p>
+        <p><b>Defesa-Especial:</b> {pokeData.stats[4].base_stat}</p>
+        <p><b>Velocidade:</b> {pokeData.stats[5].base_stat}</p>
       </section>
 
       <section>
         <h3>Tipo 1:</h3>
-        <p>Placeholder</p>
+        <p>{pokeData.types[0].type.name}</p>
         <h3>Tipo 2:</h3>
-        <p>Placeholder</p>
+        <p>{pokeData.types[1] ? pokeData.types[1].type.name : "Nenhum"}</p>
       </section>
 
       <section>
         <h2>Ataques</h2>
-        <p>Ataque 1: </p>
-        <p>Ataque 2: </p>
-        <p>Ataque 3: </p>
-        <p>Ataque 4: </p>
+        <p><b>Ataque 1:</b> {pokeData.moves[0].move.name}</p>
+        <p><b>Ataque 2:</b> {pokeData.moves[1].move.name ? pokeData.moves[1].move.name : "Nenhum"}</p>
+        <p><b>Ataque 3:</b> {pokeData.moves[2].move.name ? pokeData.moves[2].move.name : "Nenhum"}</p>
+        <p><b>Ataque 4:</b> {pokeData.moves[3].move.name ? pokeData.moves[3].move.name : "Nenhum"}</p>
       </section>
     </div>}
     </>
